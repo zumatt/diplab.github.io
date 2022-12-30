453219beadcdfdb31bb25086bbad3c2109c1c2f2
zumatt <matteo.subet@student.supsi.ch>
2022-12-30 13:48:03 +0100

    continue of control center

--- diplab-petri/examples/controlCenter/controlCenter.ino
+++ diplab-petri/examples/controlCenter/controlCenter.ino
@@ -18,17 +18,27 @@
 
 #define ssid "DiPLab"
 #define pass "password"
+#define fwdBtn_PIN 13
+#define bckBtn_PIN 12
+
+int lastState_fwd = HIGH;     // the previous state from the fwd button pin
+int currentState_fwd;         // the current reading from the fwd input pin
+int lastState_bck = HIGH;     // the previous state from the bck button pin
+int currentState_bck;         // the current reading from the bck input pin
+int counter = 0;              // the counter indicating the state of the button bck (--) / fwd (++)
 
 Inkplate display(INKPLATE_1BIT); // Create an object on Inkplate library and also set library into 1 Bit mode (BW)
 WebServer server(80);            // Create Web server on port 80 (HTTP port number)
 
 IPAddress serverIP;
-String bacteria;
+String modality;
 int dispW = 1024;                                         //Display Width
 int dispH = 758;                                          //Display Height
 int petriD = (dispH/2)-10;                                //Petri dish diameter
 int xC =  (dispW/2)-(petriD);                             //X position of left side of petri dish
 int yC =  (dispH/2)-(petriD);                             //Y position of top side of petri dish
+int xC2 = (dispW/2)+(petriD);                             //X position of right side of petri dish
+int yC2 = (dispH/2)+(petriD);                             //Y position of bottom side of petri dish
 
 void setup()
 {
@@ -48,7 +58,7 @@ void setup()
     serverIP = WiFi.softAPIP(); // Get the server IP address
 
     server.on("/", handleRoot); // If you open homepage, go to handle root function
-    server.on(UriBraces("/selection/{}"),
+    server.on(UriBraces("/mode/{}"),
               handleString); // If you send some text to Inkplate, go to handleString function. Note that {} brackets at
                              // the end of address. That means that web address has some arguments (our text!).
     server.begin();          // Start the web server
@@ -58,6 +68,25 @@ void setup()
 void loop()
 {
     server.handleClient(); // You have to constantly read if there is any new client connected to web server
+    if(modality == "history"){  
+      if(lastState_fwd == LOW && currentState_fwd == HIGH){
+        counter ++;
+        historyCondition();
+      }   
+      if(lastState_bck == LOW && currentState_bck == HIGH){
+        if (counter != 0){counter --;}
+        historyCondition();
+      }
+    } else if(modality == "microscope"){  
+      if(lastState_fwd == LOW && currentState_fwd == HIGH){
+        counter ++;
+        historyCondition();
+      }   
+      if(lastState_bck == LOW && currentState_bck == HIGH){
+        if (counter != 0){counter --;}
+        historyCondition();
+      }
+    }
 }
 
 void updateHTML()
@@ -73,9 +102,11 @@ void handleRoot()
 void handleString()
 { // This function will send response to client, send HTML code of web page, get the text from argument sent in web page
   // address and refresh screen with new text
-    bacteria = server.arg("bacteria");
+    modality = server.arg("modality");
     updateHTML();
-    updatePaper();
+    if (modality == "history"){historyMode();}
+    if (modality == "microscope"){microscopeMode();}
+    //updatePaper();
 }
 
 void updatePaper()
@@ -83,9 +114,9 @@ void updatePaper()
     display.clearDisplay();    // Clear everything from epaper frame buffer
     display.drawCircle(dispW/2, dispH/2, petriD, BLACK);
     display.setCursor(xC+100, yC+250);
-    display.println("Selected bacteria:"); // Print out what user typed in web page
+    display.println("Selected modality:"); // Print out what user typed in web page
     display.setCursor(xC+100, yC+300);
-    display.print(bacteria);
+    display.print(modality);
     display.partialUpdate(); // Send everything to screen (refresh the screen)
 }
 
@@ -107,4 +138,83 @@ void firstScreen(){
   display.print(serverIP);
   display.println('/');
   display.display();
+}
+
+void microscopeMode(){
+    counter = 0;
+    baseLayer();
+
+      display.fillCircle(xC2-100, yC+150, 50, 0);
+    display.fillCircle(xC2-100, yC+150, 25, 1);
+      display.fillCircle(xC+150, yC+450, 100, 0);
+    display.fillCircle(xC+150, yC+450, 25, 1);
+      display.fillCircle(xC+400, yC+600, 200, 0);
+    display.fillCircle(xC+400, yC+600, 25, 1);
+    display.display();
+}
+
+void historyMode(){
+    counter = 0;
+    baseLayer();
+
+    display.fillCircle(xC2-100, yC+150, 25, 1);
+    display.fillCircle(xC+150, yC+450, 25, 1);
+    display.fillCircle(xC+400, yC+600, 25, 1);
+    display.display();
+}
+
+void baseLayer(){
+    display.clearDisplay();
+      //Draw base layer
+    display.drawCircle(dispW/2, dispH/2, petriD, BLACK);
+    for(int i=1; i<=20; i++){  
+      display.drawLine(xC+(i*petriD/10), yC, xC+(i*petriD/10), yC2, BLACK);}
+    for(int a=1; a<=20; a++){  
+      display.drawLine(xC, yC+(a*petriD/10), xC2, yC+(a*petriD/10), BLACK);}
+    //END of draw base layer
+}
+
+void historyBtn(int j,k,l){
+    baseLayer();
+
+      display.fillCircle(xC2-100, yC+150, 25*j, 0);
+    display.fillCircle(xC2-100, yC+150, 25, 1);
+      display.fillCircle(xC+150, yC+450, 25*k, 0);
+    display.fillCircle(xC+150, yC+450, 25, 1);
+      display.fillCircle(xC+400, yC+600, 25*l, 0);
+    display.fillCircle(xC+400, yC+600, 25, 1);
+    display.display();
+}
+
+void historyCondition(){
+  if(counter == 0){ //0h
+    historyMode();
+  } else if(counter == 1){ //8h
+    historyBtn(1.3,2,4);
+  } else if(counter == 2){ //12h
+    historyBtn(1.6,3,6);
+  } else if(counter == 3){ //24h
+    historyBtn(2,4,8);
+  }
+}
+
+void microscopeCondition(){
+  if(counter == 0){ //0x
+    microscopeMode();
+  } else if(counter == 1){ //4x
+    baseLayer();
+      display.fillCircle(dispW/2, dispH/2, 100, 0);
+    display.fillCircle(dispW/2, dispH/2, 25, 1);
+    display.display();
+  } else if(counter == 2){ //10x
+    baseLayer();
+      display.fillCircle(dispW/2, dispH/2, 100, 0);
+    display.fillCircle(dispW/2, dispH/2, 25, 1);
+    display.display();
+  } else if(counter == 3){ //40x
+    baseLayer();
+      display.fillCircle(dispW/2, dispH/2, 100, 0);
+    display.fillCircle(dispW/2, dispH/2, 25, 1);
+    display.display();
+  }
 }
