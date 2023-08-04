8a8432eb9b29d8324e1cf014fb31e275b5b4ac27
Matteo Subet <matteo.subet@student.supsi.ch>
2023-08-04 16:45:53 +0200

    Starting work on the Beta version 2

---
+++ diplab-petri/betaVersion/betaVersion_v02-0/buttonsConditions.ino
@@ -0,0 +1,30 @@
+/*
+    -----------------------------------------------------------
+                          BTN CHECK
+    -----------------------------------------------------------
+*/
+
+void buttonsCondition(){
+if(j_controlCenter == "history"){  
+      if(lastState_fwd == LOW && currentState_fwd == HIGH){
+        if (counter < 3){counter ++;}
+        historyCondition();
+      }   
+      if(lastState_bck == LOW && currentState_bck == HIGH){
+        if (counter != 0){counter --;}
+        historyCondition();
+      }
+    } else if(j_controlCenter == "microscope"){  
+      if(lastState_fwd == LOW && currentState_fwd == HIGH){
+        if (counter < 3){counter ++;}
+        microscopeCondition();
+      }   
+      if(lastState_bck == LOW && currentState_bck == HIGH){
+        if (counter != 0){counter --;}
+        microscopeCondition();
+      }
+    }
+
+    lastState_fwd = currentState_fwd;
+    lastState_bck = currentState_bck;
+    }
