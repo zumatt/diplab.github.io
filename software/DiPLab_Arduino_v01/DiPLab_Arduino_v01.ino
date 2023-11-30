54649fcb386a1ee992384c54d5b540ab0bb85ae7
Matteo Subet <matteo.subet@student.supsi.ch>
2023-11-30 15:03:50 +0100

    Fix on resistance calculation

--- software/DiPLab_Arduino_v01/DiPLab_Arduino_v01.ino
+++ software/DiPLab_Arduino_v01/DiPLab_Arduino_v01.ino
@@ -3,7 +3,7 @@
    Release Version 0.1 - DiPLaB
    Spearhead Research Project - https://spearhead-amr.github.io/makeaware/
    
-        Coded by Matteo Subet - Last version of 15 November 2023
+        Coded by Matteo Subet - Last version of 30 November 2023
    
    SUPSI - University of Applied Sciences and Arts of Southern Switzerland
    DACD - Department of Architecture, Construction and Design
@@ -1075,94 +1075,52 @@ void checkResistance(){
   Serial.print("  - Antibiotic 3: ");
   Serial.println(webAb3);
 
-  ab1Resistance = abMmToPxMultiplier * gatifloxacin[0];
-  ab2Resistance = abMmToPxMultiplier * levofloxacin[0];
-  ab3Resistance = abMmToPxMultiplier * minocycine[0];
-/*
-  if(j_bacteria == "Acinetobacter"){
-    if (j_ab1 == "Ciprofloxacin"){ ab1_resistance = ab_resistance_multiplier * ciprofloxacin[0];};
-    if (j_ab1 == "Doxycycline"){ ab1_resistance = ab_resistance_multiplier * doxycycline[0];};
-    if (j_ab1 == "Gatifloxacin"){ ab1_resistance = ab_resistance_multiplier * gatifloxacin[0];};
-    if (j_ab1 == "Levofloxacin"){ ab1_resistance = ab_resistance_multiplier * levofloxacin[0];};
-    if (j_ab1 == "Minocycine"){ ab1_resistance = ab_resistance_multiplier * minocycine[0];};
-    if (j_ab1 == "Tetracycline"){ ab1_resistance = ab_resistance_multiplier * tetracycline[0];};
+       if(webAb1 == "Ampicillin"  ){ab1Resistance = abMmToPxMultiplier * Ampicillin}
+  else if(webAb1 == "Piperacillin"){ab1Resistance = abMmToPxMultiplier * Piperacillin}
+  else if(webAb1 == "Mecillinam"  ){ab1Resistance = abMmToPxMultiplier * Mecillinam}
+  else if(webAb1 == "Cefadroxil"  ){ab1Resistance = abMmToPxMultiplier * Cefadroxil}
+  else if(webAb1 == "Cefalexin"   ){ab1Resistance = abMmToPxMultiplier * Cefalexin}
+  else if(webAb1 == "Cefixime"    ){ab1Resistance = abMmToPxMultiplier * Cefixime}
+  else if(webAb1 == "Cefotaxime"  ){ab1Resistance = abMmToPxMultiplier * Cefotaxime}
+  else if(webAb1 == "Cefoxitin"   ){ab1Resistance = abMmToPxMultiplier * Cefoxitin}
+  else if(webAb1 == "Cefpodoxime" ){ab1Resistance = abMmToPxMultiplier * Cefpodoxime}
+  else if(webAb1 == "Ceftazidime" ){ab1Resistance = abMmToPxMultiplier * Ceftazidime}
+  else if(webAb1 == "Cefuroxime"  ){ab1Resistance = abMmToPxMultiplier * Cefuroxime}
+  else if(webAb1 == "Imipenem"    ){ab1Resistance = abMmToPxMultiplier * Imipenem}
+  else if(webAb1 == "Amikacin"    ){ab1Resistance = abMmToPxMultiplier * Amikacin}
+  else if(webAb1 == "Gentamicin"  ){ab1Resistance = abMmToPxMultiplier * Gentamicin}
+  else if(webAb1 == "Tobramycin"  ){ab1Resistance = abMmToPxMultiplier * Tobramycin}
 
-    if (j_ab2 == "Ciprofloxacin"){ ab2_resistance = ab_resistance_multiplier * ciprofloxacin[0];};
-    if (j_ab2 == "Doxycycline"){ ab2_resistance = ab_resistance_multiplier * doxycycline[0];};
-    if (j_ab2 == "Gatifloxacin"){ ab2_resistance = ab_resistance_multiplier * gatifloxacin[0];};
-    if (j_ab2 == "Levofloxacin"){ ab2_resistance = ab_resistance_multiplier * levofloxacin[0];};
-    if (j_ab2 == "Minocycine"){ ab2_resistance = ab_resistance_multiplier * minocycine[0];};
-    if (j_ab2 == "Tetracycline"){ ab2_resistance = ab_resistance_multiplier * tetracycline[0];};
-    
-    if (j_ab3 == "Ciprofloxacin"){ ab3_resistance = ab_resistance_multiplier * ciprofloxacin[0];};
-    if (j_ab3 == "Doxycycline"){ ab3_resistance = ab_resistance_multiplier * doxycycline[0];};
-    if (j_ab3 == "Gatifloxacin"){ ab3_resistance = ab_resistance_multiplier * gatifloxacin[0];};
-    if (j_ab3 == "Levofloxacin"){ ab3_resistance = ab_resistance_multiplier * levofloxacin[0];};
-    if (j_ab3 == "Minocycine"){ ab3_resistance = ab_resistance_multiplier * minocycine[0];};
-    if (j_ab3 == "Tetracycline"){ ab3_resistance = ab_resistance_multiplier * tetracycline[0];};
-  } else if(j_bacteria == "Enterococcus"){
-    if (j_ab1 == "Ciprofloxacin"){ ab1_resistance = ab_resistance_multiplier * ciprofloxacin[1];};
-    if (j_ab1 == "Doxycycline"){ ab1_resistance = ab_resistance_multiplier * doxycycline[1];};
-    if (j_ab1 == "Gatifloxacin"){ ab1_resistance = ab_resistance_multiplier * gatifloxacin[1];};
-    if (j_ab1 == "Levofloxacin"){ ab1_resistance = ab_resistance_multiplier * levofloxacin[1];};
-    if (j_ab1 == "Minocycine"){ ab1_resistance = ab_resistance_multiplier * minocycine[1];};
-    if (j_ab1 == "Tetracycline"){ ab1_resistance = ab_resistance_multiplier * tetracycline[1];};
 
-    if (j_ab2 == "Ciprofloxacin"){ ab2_resistance = ab_resistance_multiplier * ciprofloxacin[1];};
-    if (j_ab2 == "Doxycycline"){ ab2_resistance = ab_resistance_multiplier * doxycycline[1];};
-    if (j_ab2 == "Gatifloxacin"){ ab2_resistance = ab_resistance_multiplier * gatifloxacin[1];};
-    if (j_ab2 == "Levofloxacin"){ ab2_resistance = ab_resistance_multiplier * levofloxacin[1];};
-    if (j_ab2 == "Minocycine"){ ab2_resistance = ab_resistance_multiplier * minocycine[1];};
-    if (j_ab2 == "Tetracycline"){ ab2_resistance = ab_resistance_multiplier * tetracycline[1];};
-    
-    if (j_ab3 == "Ciprofloxacin"){ ab3_resistance = ab_resistance_multiplier * ciprofloxacin[1];};
-    if (j_ab3 == "Doxycycline"){ ab3_resistance = ab_resistance_multiplier * doxycycline[1];};
-    if (j_ab3 == "Gatifloxacin"){ ab3_resistance = ab_resistance_multiplier * gatifloxacin[1];};
-    if (j_ab3 == "Levofloxacin"){ ab3_resistance = ab_resistance_multiplier * levofloxacin[1];};
-    if (j_ab3 == "Minocycine"){ ab3_resistance = ab_resistance_multiplier * minocycine[1];};
-    if (j_ab3 == "Tetracycline"){ ab3_resistance = ab_resistance_multiplier * tetracycline[1];};
-  } else if(j_bacteria == "Entrobacteriaceae"){
-    if (j_ab1 == "Ciprofloxacin"){ ab1_resistance = ab_resistance_multiplier * ciprofloxacin[2];};
-    if (j_ab1 == "Doxycycline"){ ab1_resistance = ab_resistance_multiplier * doxycycline[2];};
-    if (j_ab1 == "Gatifloxacin"){ ab1_resistance = ab_resistance_multiplier * gatifloxacin[2];};
-    if (j_ab1 == "Levofloxacin"){ ab1_resistance = ab_resistance_multiplier * levofloxacin[2];};
-    if (j_ab1 == "Minocycine"){ ab1_resistance = ab_resistance_multiplier * minocycine[2];};
-    if (j_ab1 == "Tetracycline"){ ab1_resistance = ab_resistance_multiplier * tetracycline[2];};
+       if(webAb2 == "Ampicillin"  ){ab2Resistance = abMmToPxMultiplier * Ampicillin}
+  else if(webAb2 == "Piperacillin"){ab2Resistance = abMmToPxMultiplier * Piperacillin}
+  else if(webAb2 == "Mecillinam"  ){ab2Resistance = abMmToPxMultiplier * Mecillinam}
+  else if(webAb2 == "Cefadroxil"  ){ab2Resistance = abMmToPxMultiplier * Cefadroxil}
+  else if(webAb2 == "Cefalexin"   ){ab2Resistance = abMmToPxMultiplier * Cefalexin}
+  else if(webAb2 == "Cefixime"    ){ab2Resistance = abMmToPxMultiplier * Cefixime}
+  else if(webAb2 == "Cefotaxime"  ){ab2Resistance = abMmToPxMultiplier * Cefotaxime}
+  else if(webAb2 == "Cefoxitin"   ){ab2Resistance = abMmToPxMultiplier * Cefoxitin}
+  else if(webAb2 == "Cefpodoxime" ){ab2Resistance = abMmToPxMultiplier * Cefpodoxime}
+  else if(webAb2 == "Ceftazidime" ){ab2Resistance = abMmToPxMultiplier * Ceftazidime}
+  else if(webAb2 == "Cefuroxime"  ){ab2Resistance = abMmToPxMultiplier * Cefuroxime}
+  else if(webAb2 == "Imipenem"    ){ab2Resistance = abMmToPxMultiplier * Imipenem}
+  else if(webAb2 == "Amikacin"    ){ab2Resistance = abMmToPxMultiplier * Amikacin}
+  else if(webAb2 == "Gentamicin"  ){ab2Resistance = abMmToPxMultiplier * Gentamicin}
+  else if(webAb2 == "Tobramycin"  ){ab2Resistance = abMmToPxMultiplier * Tobramycin}
 
-    if (j_ab2 == "Ciprofloxacin"){ ab2_resistance = ab_resistance_multiplier * ciprofloxacin[2];};
-    if (j_ab2 == "Doxycycline"){ ab2_resistance = ab_resistance_multiplier * doxycycline[2];};
-    if (j_ab2 == "Gatifloxacin"){ ab2_resistance = ab_resistance_multiplier * gatifloxacin[2];};
-    if (j_ab2 == "Levofloxacin"){ ab2_resistance = ab_resistance_multiplier * levofloxacin[2];};
-    if (j_ab2 == "Minocycine"){ ab2_resistance = ab_resistance_multiplier * minocycine[2];};
-    if (j_ab2 == "Tetracycline"){ ab2_resistance = ab_resistance_multiplier * tetracycline[2];};
-    
-    if (j_ab3 == "Ciprofloxacin"){ ab3_resistance = ab_resistance_multiplier * ciprofloxacin[2];};
-    if (j_ab3 == "Doxycycline"){ ab3_resistance = ab_resistance_multiplier * doxycycline[2];};
-    if (j_ab3 == "Gatifloxacin"){ ab3_resistance = ab_resistance_multiplier * gatifloxacin[2];};
-    if (j_ab3 == "Levofloxacin"){ ab3_resistance = ab_resistance_multiplier * levofloxacin[2];};
-    if (j_ab3 == "Minocycine"){ ab3_resistance = ab_resistance_multiplier * minocycine[2];};
-    if (j_ab3 == "Tetracycline"){ ab3_resistance = ab_resistance_multiplier * tetracycline[2];};
-  }
-
-  ab1_resistanceWeb = 1.6 * 40 * ((ab1_resistance * ab_resistance_multiplier * abDiameter) / 35);
-  ab2_resistanceWeb = 1.6 * 40 * ((ab2_resistance * ab_resistance_multiplier * abDiameter) / 35);
-  ab3_resistanceWeb = 1.6 * 40 * ((ab3_resistance * ab_resistance_multiplier * abDiameter) / 35);
-
-  Serial.println("");
-  Serial.println(" ---------------- ");
-  Serial.println("");
-  Serial.println("Data produced:");
-  Serial.print("  - Resistance value 1: ");
-  Serial.println(ab1_resistance);
-  Serial.print("  - Resistance web value 1: ");
-  Serial.println(ab1_resistanceWeb);
-  Serial.print("  - Resistance value 2: ");
-  Serial.println(ab2_resistance);
-  Serial.print("  - Resistance web value 2: ");
-  Serial.println(ab2_resistanceWeb);
-  Serial.print("  - Resistance value 3: ");
-  Serial.println(ab3_resistance);
-  Serial.print("  - Resistance web value 3: ");
-  Serial.println(ab3_resistanceWeb);
-*/
+       if(webAb3 == "Ampicillin"  ){ab3Resistance = abMmToPxMultiplier * Ampicillin}
+  else if(webAb3 == "Piperacillin"){ab3Resistance = abMmToPxMultiplier * Piperacillin}
+  else if(webAb3 == "Mecillinam"  ){ab3Resistance = abMmToPxMultiplier * Mecillinam}
+  else if(webAb3 == "Cefadroxil"  ){ab3Resistance = abMmToPxMultiplier * Cefadroxil}
+  else if(webAb3 == "Cefalexin"   ){ab3Resistance = abMmToPxMultiplier * Cefalexin}
+  else if(webAb3 == "Cefixime"    ){ab3Resistance = abMmToPxMultiplier * Cefixime}
+  else if(webAb3 == "Cefotaxime"  ){ab3Resistance = abMmToPxMultiplier * Cefotaxime}
+  else if(webAb3 == "Cefoxitin"   ){ab3Resistance = abMmToPxMultiplier * Cefoxitin}
+  else if(webAb3 == "Cefpodoxime" ){ab3Resistance = abMmToPxMultiplier * Cefpodoxime}
+  else if(webAb3 == "Ceftazidime" ){ab3Resistance = abMmToPxMultiplier * Ceftazidime}
+  else if(webAb3 == "Cefuroxime"  ){ab3Resistance = abMmToPxMultiplier * Cefuroxime}
+  else if(webAb3 == "Imipenem"    ){ab3Resistance = abMmToPxMultiplier * Imipenem}
+  else if(webAb3 == "Amikacin"    ){ab3Resistance = abMmToPxMultiplier * Amikacin}
+  else if(webAb3 == "Gentamicin"  ){ab3Resistance = abMmToPxMultiplier * Gentamicin}
+  else if(webAb3 == "Tobramycin"  ){ab3Resistance = abMmToPxMultiplier * Tobramycin}
 }
